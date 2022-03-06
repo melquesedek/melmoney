eed30d381435286dfb5da7d5080e3bf3900c6a20
Melquesedek Antonio de Oliveira <melquesedekdeoliveira@gmail.com>
2022-03-06 11:24:33 -0300

    Ajustes no Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,8 +11,10 @@ export function Header({onOpenNewTransactionModal}: HeaderProps){
         <Container>
             <Content>
             <img src={logoImg} alt="mel money" />
-            <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
-            
+            <button type="button"
+            onClick={onOpenNewTransactionModal}>
+                Nova transação
+                </button>
             </Content>
         </Container>
     );
