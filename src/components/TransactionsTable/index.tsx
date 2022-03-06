fc3a59b98c7b1d63569ca5998bc3ef7ae6dfa9ad
Melquesedek Antonio de Oliveira <melquesedekdeoliveira@gmail.com>
2022-03-06 10:23:12 -0300

    Implementando MirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable(){
+    useEffect(() => {
+        fetch('http://172.24.253.61:3000/api/transactions')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    },[])
+
     return(
         <Container>
             <table>
