8843e0ebd72cb7f75dc05cc737999893c95e7586
Melquesedek Antonio de Oliveira <melquesedekdeoliveira@gmail.com>
2022-03-06 12:27:28 -0300

    Estilização do Modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -74,4 +74,16 @@ button{
     position: relative;
     border-radius: 0.25rem;
 }
+.react-modal-close{
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+    &:hover{
+        filter: brightness(0.8);
+    }
+}
 `
